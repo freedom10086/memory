2025b48d15b230cfc67a097e4ab81de3293248ab
yang <2351386755@qq.com>
2018-05-31 17:34:54 +0800

    message

---
+++ src/main/java/com/tencent/memory/model/Message.java
@@ -0,0 +1,13 @@
+package com.tencent.memory.model;
+
+public class Message {
+
+    public int type; //0--comment 1--like
+
+    public Image image;
+
+    public User creater;
+
+
+
+}
---
+++ src/main/java/com/tencent/memory/model/MessageType.java
@@ -0,0 +1,6 @@
+package com.tencent.memory.model;
+
+public enum  MessageType {
+    COMMENT, // 评论
+    LIKE //点赞
+}
